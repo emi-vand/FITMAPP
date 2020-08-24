fb550d3722e8fee8accb2e8ac3ecf0f787eb543b
Denisa Milian <denisa.g.milian@gmail.com>
2020-08-24 17:54:03 +0100

    Created 3 gyms

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+gym1= Gym.create(name:'Third Space Tower Bridge', location:'2b More London Riverside, London SE1 2AP
+', category:'General', user_id:4)
+gym2= Gym.create(name:'Third Space Soho', location:'67 Brewer St, Soho, London W1F 9US', category:'General', user_id:4)
+gym3= Gym.create(name: 'London Boxing Classes', location:'87-89 Shepperton Rd, Islington, London N1 3DF', user_id:5)
