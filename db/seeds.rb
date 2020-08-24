eacc189eaf00e3839ed1b5d9036338b4b61835e5
Matt Hardy <matthewhardy17@gmail.com>
2020-08-24 17:54:05 +0100

    created restaurant seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "creating restaurants"
+
+hellosalad = Restaurant.create!(
+  name: "Hello Salad",
+  location: "Shoreditch, London",
+  category: "Salads",
+  user_id: 4
+)
+
+wagonwok = Restaurant.create!(
+  name: "Wagon Wok",
+  location: "Hoxton, London",
+  category: "Asian",
+  user_id: 5
+)
+
+sushisamaba = Restaurant.create!(
+  name: "Sushi Samaba",
+  location: "Liverpool Street, London",
+  category: "Sushi",
+  user_id: 5
+)
+
+puts "finished"
