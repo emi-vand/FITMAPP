fe0ead6f021bd134984ca18f9f174bda1618ad6a
Denisa Milian <denisa.g.milian@gmail.com>
2020-08-28 12:17:08 +0100

    Done with the filtering system and modified the method for the tags to update

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -8,8 +8,8 @@ class Restaurant < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
-
   acts_as_favoritable
   acts_as_taggable_on :dietary
 
+  DIETARY = ["Vegan", "Vegetarian", "Keto", "Paleo", "Pescaritan", "Gluten-free", "Lactose-free", "Dairy-free"]
 end
