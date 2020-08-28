3fad0449d1ef291baea33b7d043638b067bd961a
Emilia Vandamme <emilia.audrey@gmail.com>
2020-08-28 11:46:18 +0100

    fixed javascript booking

--- app/javascript/plugins/modal_restaurant_booking.js
+++ app/javascript/plugins/modal_restaurant_booking.js
@@ -3,14 +3,14 @@ export const restaurantBookingModal = () => {
   const modal = document.querySelector(".modal-body");
   if (btn) {
     btn.addEventListener("click", () => {
-      modal.innerHTML = `<div class="modal-body">
+      modal.lastElementChild.style.display = "none"
+      modal.insertAdjacentHTML("beforeEnd", `<div class="modal-body">
                           <p class="text-center">Your booking has been confirmed. Check your email for details.</p>
                         </div>
                         <div class="modal-footer">
-                          <a class="btn btn-primary btn-sm" href="/dashboard">My Bookings</a>
                           <a class="btn btn-secondary btn-sm" href="/restaurants">Back to Restaurants</a>
-
-                        </div>`;
+                          <a class="btn btn-primary btn-sm" href="/dashboard">My Bookings</a>
+                        </div>`);
     })
   }
 }
